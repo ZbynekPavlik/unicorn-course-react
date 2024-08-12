433ebe324fdb270428458c69bd87ebd34e489509
Zbynek Pavlik <zbynek.pavlik@unicorn.com>
2024-08-12 09:26:05 +0200

    Chapter 2 - task #3 - oprava - pridani key name.id

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,25 +1,14 @@
 import Recipe from "./Recipe";
 import {Container, Row, Col} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './RecipeList.css'; // Import the CSS file
+import './RecipeList.css';
 
 function RecipeList(props) {
     function getRecipeList(recipeList) {
         return recipeList.map((recipe) => {
             return (
-                <>
-                    {!props.isBiggerSpacing &&
-                        <Col xs={12} sm={6} md={4} lg={4} key={recipe.name} className="card-spacing">
-                            <Recipe recipe={recipe}/>
-                        </Col>}
-
-                    {props.isBiggerSpacing &&
-                        <Col xs={12} sm={6} md={4} lg={4} key={recipe.name} className="card-bigger-spacing">
-                            <Recipe recipe={recipe}/>
-                        </Col>}
-
-
-                </>
+                <Col xs={12} sm={6} md={4} lg={4} key={recipe.id} className={props.isBiggerSpacing ? "card-bigger-spacing" : "card-spacing" }>
+                    <Recipe recipe={recipe}/>
+                </Col>
             );
         });
     }
