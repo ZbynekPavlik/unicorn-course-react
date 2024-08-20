a9834573f378de0662f69fbb78d5e0c210ee3a15
Zbynek Pavlik <zbynek.pavlik@unicorn.com>
2024-08-20 11:29:53 +0200

    Chapter 5 - task #1 #2 - oprava bug, zobrazeni mene textu se nezobrazoval list ingredienci a obrazek byl velky

--- src/components/RecipeGridList.jsx
+++ src/components/RecipeGridList.jsx
@@ -5,8 +5,8 @@ import {Col, Container, Row} from "react-bootstrap";
 
 function RecipeGridList({recipeList, ingredientList, isBiggerSpacing, isSmallDetail}) {
     return (
-        <Container className="d-flex justify-content-center">
-            <Row className="justify-content-center no-gutters">
+        <Container>
+            <Row>
                 {recipeList.map((recipe) => (
                     <Col xs={12} sm={6} md={6} lg={4} key={recipe.id}
                          className={isBiggerSpacing ? "card-bigger-spacing" : "card-spacing"}>
